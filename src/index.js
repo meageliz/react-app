fa430fdb63fcd81ef5c42bc0c4ce7aea49b23551
meageliz <92804633+meageliz@users.noreply.github.com>
2022-02-25 10:55:24 +0100

    Fixed css files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import axios from "axios";
 
 ReactDOM.render(
   <React.StrictMode>
