fc267ae52a389fb9995a35ef5fe8520079f5161c
meageliz <92804633+meageliz@users.noreply.github.com>
2022-02-25 11:12:29 +0100

    Added link to github code

--- src/CoderCred.js
+++ src/CoderCred.js
@@ -2,6 +2,15 @@ import react from "React";
 
 export default function CoderCred() {
   return (
-    <div className="CoderCred">Open-source code created by Meag Gardner</div>
+    <div className="CoderCred">
+      <a
+        href="https://github.com/meageliz/react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>{" "}
+      created by Meag Gardner
+    </div>
   );
 }
