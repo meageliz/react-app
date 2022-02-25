b5f5ea6938d834b534dfb7200dbfb218a348af02
meageliz <92804633+meageliz@users.noreply.github.com>
2022-02-25 11:10:28 +0100

    Created component for coder cred

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Search from "./Search";
+import CoderCred from "./CoderCred";
 
 export default function App() {
   return (
     <div className="App">
       <Search weather="{weather}" city="{city}" />
+      <CoderCred />
     </div>
   );
 }
---
+++ src/CoderCred.js
@@ -0,0 +1,7 @@
+import react from "React";
+
+export default function CoderCred() {
+  return (
+    <div className="CoderCred">Open-source code created by Meag Gardner</div>
+  );
+}
